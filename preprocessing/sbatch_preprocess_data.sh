52bb5f756c1324393c6144fc58376392ad994526
Jerry Lin <jlin96@br012.ib.bridges2.psc.edu>
2024-02-27 15:49:23 -0500

    edited sbatch script

--- preprocessing/sbatch_preprocess_data.sh
+++ preprocessing/sbatch_preprocess_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name="preprocess_data_specific"
+#SBATCH --job-name="preprocess_data_relative"
 #SBATCH --output="preprocess_data.out"
 #SBATCH --partition=RM-512
 #SBATCH --export=ALL
 #SBATCH --account=atm200007p
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jerryl9@uci.edu
-#SBATCH -t 4:15:00
+#SBATCH -t 2:10:00
 
 source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load anaconda3
